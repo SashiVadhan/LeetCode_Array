0d47e6ff3033576f3817862d237f11bcb5f7a862
Sashi Vadhan Yennamsetty <subscriptions.sashi@gmail.com>
2026-09-01 19:16:18 +0530

    Delete Two Sum.py

--- Two Sum.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def twoSum(self, nums: list, target: int):
-        value={}
-        for i in range(len(nums)):
-            remaining= target - nums[i]
-            if remaining in value:
-                return [value[remaining], i]
-            else:
-                value[nums[i]]=i
