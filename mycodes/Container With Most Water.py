6c21477ad822859d3b77d6eabb2ef44cf5f63323
Sashi Vadhan Yennamsetty <subscriptions.sashi@gmail.com>
2026-09-04 18:37:01 +0530

    maxArea function for Container With Most Water
    Implement maxArea function to calculate maximum water container area.

---
+++ mycodes/Container With Most Water.py
@@ -0,0 +1,32 @@
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        maxi = 0
+        L, R = 0, len(height) - 1
+        tallest = max(height)
+        while L < R:
+            area = (R - L)*min(height[L], height[R])
+            maxi = max(maxi,area)
+            if height[L] < height[R]:
+                L += 1
+            else:
+                R -= 1
+            if tallest * (R - L) <= maxi:
+                break
+
+        return maxi
+
+# explanation:
+"""
+This code also runs without checking the tallest, i.e. without checking
+the condition "tallest * (R - L) <= maxi".
+
+But in that case, the time taken can be higher because we have to check
+all the remaining possible positions even when we already know that
+none of them can give a better answer.  ->  (53ms)
+
+The "tallest * (R - L) <= maxi" condition helps us stop early when the
+maximum possible area from the remaining width cannot exceed maxi.
+"""
+# Time Complexity: O(n)
+# 3ms
+# 29.4mb
